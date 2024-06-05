40cdaa41e8443c438773b7084e44818b4b6097a6
saddydead <luckybeyhuk@gmail.com>
2024-06-05 23:50:40 +0300

    0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 setup(
   include_package_data=True,
   name='minebannerlib',
-  version='0.1.0a3',
+  version='0.1.0',
   author='saddydead1',
   author_email='saddydead1@gmail.com',
   description='Lib for create minecraft banner image',
@@ -17,6 +17,7 @@ setup(
   long_description_content_type='text/markdown',
   url='https://github.com/saddydead1/minebannerlib',
   packages=find_packages(),
+  package_data={'': ['*.png', 'samples/*.png']},
   install_requires=['requests>=2.25.1','nbtlib', 'pillow', 'multipledispatch'],
   classifiers=[
     'Programming Language :: Python :: 3.11',
