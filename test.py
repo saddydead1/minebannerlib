aaef4da6b36afd8b35571de5b1b4dd33ee9e73a8
saddydead <luckybeyhuk@gmail.com>
2024-06-05 22:52:01 +0300

    0.1.0a сююдааа

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from MineBannerLib import create_banner as mbl
+from MineBannerLib.create_banner import create_banner
 from MineBannerLib.layer import Layer
 
 nbts = '{BlockEntityTag:{Patterns:[{Color:14,Pattern:"cre"},{Color:4,Pattern:"sku"}]}}'
@@ -9,11 +9,11 @@ parts = [Layer('hh', 2), Layer('mc', 1), Layer('cre', 3)]
 
 
 def test1():
-    mbl.create_banner(name, nbts, color_base)
+    create_banner(name, nbts, color_base)
 
 
 def test2():
-    mbl.create_banner(name2, parts, color_base)
+    create_banner(name2, parts, color_base)
 
 
 if __name__ == '__main__':
